02c84508bd000afd0158d75e0a1624e052e42c61
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2025-03-03 16:23:42 +0200

    add user controller

---
+++ server/src/controllers/userController.js
@@ -0,0 +1,45 @@
+import userService from "../services/userService";
+
+const register = async (req, res) => {
+    const { username, email, password, rePassword } = req.body;
+
+    try {
+        const newUser = await userService.registerUser(username, email, password, rePassword);
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const userData = await userService.loginUser(email, password);
+        res.status(200).json(userData);
+    } catch (error) {
+        res.status(401).json({ message: error.message });
+    }
+};
+
+const logout = (req, res) => {
+    res.status(200).json({ message: "Logout successful. Remove token from client storage." });
+};
+
+const getProfile = async (req, res) => {
+    const userId = req.user.userId;
+
+    try {
+        const userProfile = await userService.getUserProfile(userId);
+        res.status(200).json(userProfile);
+    } catch (error) {
+        res.status(404).json({ message: error.message });   
+    }
+};
+
+export default {
+    register,
+    login,
+    logout,
+    getProfile,
+};
