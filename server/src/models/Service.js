7d99870a7794aff93cec65fc9259a9bb569d81d8
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2025-03-03 15:12:05 +0200

    add service model

---
+++ server/src/models/Service.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const serviceSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+export default mongoose.model('Service', serviceSchema);
