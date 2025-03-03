6872b3bc5fd38f39bd74b519afaab7623a1b750a
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2025-03-03 15:58:26 +0200

    add user service with registration, login, and profile retrieval functionalities

---
+++ server/src/services/userService.js
@@ -0,0 +1,90 @@
+import User from "../models/User";
+import Booking from "../models/Booking";
+import bcrypt from "bcryptjs";
+import jwt from "../utils/jwt.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const generateToken = (user) => {
+    return jwt.sign({
+        userId: user._id,
+        email: user.email,
+        username: user.username,
+        role: user.role
+    }, process.env.JWT_SECRET, { expiresIn: '1d' });
+};
+
+const registerUser = async (username, email, password, rePassword) => {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        throw new Error('A user with this email already exists');
+    }
+
+    if (rePassword !== password) {
+        throw new Error('Passwords do not match');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({ username, email, password: hashedPassword, role: "user" });
+
+    return {
+        id: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+    };
+};
+
+const loginUser = async (email, password) => {
+    const user = await User.findOne({ email });
+    if (!user) {
+        throw new Error('Invalid email or password');
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        throw new Error('Invalid email or password');
+    }
+
+    const token = await generateToken(user);
+
+    return {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        token,
+    };
+};
+
+const getUserProfile = async (userId) => {
+    const user = await User.findById(userId).select('-password');
+
+    if(!user) {
+        throw new Error('User not found');
+    }
+
+    const bookings = await Booking.find({ user: userId }).populate('service');
+
+    return {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        role: user.role,
+        servicesUsed: bookings.map(booking => ({
+            serviceId: booking.service._id,
+            name: booking.service.name,
+            description: booking.service.description,
+            price: booking.service.price,
+            date: booking.date,
+            time: booking.time,
+            status: booking.status,
+        })),
+    };
+};
+
+export default {
+    registerUser,
+    loginUser,
+    getUserProfile,
+};
