9a18734aa6bf4a47e5c969d90e7e8f6548896070
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2025-03-03 15:26:44 +0200

    add middlewares

---
+++ server/src/middlewares/authMiddleware.js
@@ -0,0 +1,31 @@
+import jwt from "./../utils/jwt.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const authMiddleware = (req, res, next) => {
+    try {
+        const token = req.headers.authorization?.split(" ")[1];
+
+        if (!token) {
+            return res.status(401).json({ message: "Not authenticated. Token is missing." });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+
+        req.isAdmin = decoded.role === "admin";
+
+        next();
+    } catch (error) {
+        if (error.name === "JsonWebTokenError") {
+            return res.status(401).json({ message: "Invalid token" });
+        } else if (error.name === "TokenExpiredError") {
+            return res.status(401).json({ message: "Token has expired" });
+        } else {
+            return res.status(500).json({ message: "Internal server error" });
+        }
+    }
+};
+
+export default authMiddleware;
