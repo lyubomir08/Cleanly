4cfcbd79b0358bbe8ddc3aecd607118b41fd90fa
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2025-03-07 16:53:30 +0200

    add booking service logic

---
+++ server/src/services/bookingService.js
@@ -0,0 +1,47 @@
+import Booking from "../models/Booking.js";
+
+const createBooking = async (userId, serviceId, date, time) => {
+    return await Booking.create({ user: userId, service: serviceId, date, time, status: "pending" });
+};
+
+const getPendingBookings = async () => {
+    return await Booking.find({ status: "pending" }).populate("user service");
+};
+
+const updateBookingStatus = async (bookingId, status) => {
+    const booking = await Booking.findByIdAndUpdate(bookingId, { status }, { new: true });
+
+    if (!booking) {
+        throw new Error("Booking not found");
+    }
+
+    return booking;
+};
+
+const deleteUserBooking = async (userId, bookingId) => {
+    const booking = await Booking.findOneAndDelete({ _id: bookingId, user: userId });
+
+    if (!booking) {
+        throw new Error("Booking not found or access denied");
+    }
+
+    return { message: "Booking deleted successfully" };
+};
+
+const deleteBooking = async (bookingId) => {
+    const deletedBooking = await Booking.findByIdAndDelete(bookingId);
+
+    if (!deletedBooking) {
+        throw new Error("Booking not found");
+    }
+
+    return { message: "Booking deleted successfully" };
+};
+
+export default {
+    createBooking,
+    getPendingBookings,
+    updateBookingStatus,
+    deleteUserBooking,
+    deleteBooking,
+};
