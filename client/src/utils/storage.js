4e7c65461bcd473bb7f3a1351ce83f0b82a0705b
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2025-03-13 10:14:26 +0200

    add storage util functions

---
+++ client/src/utils/storage.js
@@ -0,0 +1,23 @@
+// Запазване на потребителските данни
+export function setUserData(userData) {
+    localStorage.setItem("user", JSON.stringify(userData));
+    localStorage.setItem("token", userData.token);
+}
+
+export function getUserData() {
+    return JSON.parse(localStorage.getItem("user"));
+}
+
+export function getToken() {
+    return localStorage.getItem("token");
+}
+
+export function getUserRole() {
+    const user = getUserData();
+    return user ? user.role : "guest";
+}
+
+export function clearUserData() {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+}
