24eaaa1cc0da7db3f6627544c0512f017898f0ce
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2025-03-14 20:58:22 +0200

    add serviceCreate logic

---
+++ client/src/components/service/service-create/ServiceCreate.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { createService } from "../../../services/serviceService";
+
+export default function ServiceCreate() {
+    const [formData, setFormData] = useState({
+        name: "",
+        description: "",
+        price: "",
+    });
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            await createService(formData);
+            
+            navigate("/");
+        } catch (error) {
+            console.error("Error creating service:", error);
+        }
+    };
+
+    return (
+        <div className="flex items-center justify-center py-12 w-full min-h-screen">
+            <div className="bg-white p-8 rounded-lg shadow-xl shadow-gray-400/50 w-96">
+                <h2 className="text-2xl font-semibold text-center mb-6 text-gray-800">Create New Service</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-4">
+                        <label className="block text-gray-700">Service Name</label>
+                        <input
+                            type="text"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
+                            required
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700">Description</label>
+                        <textarea
+                            name="description"
+                            value={formData.description}
+                            onChange={handleChange}
+                            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
+                            required
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700">Price</label>
+                        <input
+                            type="number"
+                            name="price"
+                            value={formData.price}
+                            onChange={handleChange}
+                            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
+                            required
+                        />
+                    </div>
+                    <button className="w-full bg-gray-800 text-white py-2 rounded-md hover:bg-gray-700 transition">
+                        Create Service
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+}
