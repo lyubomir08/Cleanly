0120baabea955e9e1163b2690419378c1d9f9faf
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2025-03-14 18:48:16 +0200

    modify footer css

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-        <footer className="bg-gray-800 text-gray-200 text-center py-4 mt-10">
-            <p>&copy; {new Date().getFullYear()} Cleanly. All rights reserved.</p>
+        <footer className="bg-gray-800 text-gray-300 text-center py-4">
+            <p>&copy; {new Date().getFullYear()} Cleanify. All rights reserved.</p>
         </footer>
     );
 }
