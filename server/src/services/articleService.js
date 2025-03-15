e8fc6adad89d512216df72cb36bf0bf3f982b237
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2025-03-15 20:42:17 +0200

    create service for articles

---
+++ server/src/services/articleService.js
@@ -0,0 +1,47 @@
+import Article from "../models/Article";
+
+const createArticle = async (title, content, author) => {
+    return await Article.create({ title, content, author });
+};
+
+const getAllArticles = async () => {
+    return await Article.find().populate('author', 'username email');
+};
+
+const getArticleById = async (id) => {
+    const article = await Article.findById(id).populate('author', 'username email');
+    if (!article) throw new Error('Article not found');
+
+    return article;
+};
+
+const updateArticle = async (id, userId, updateData) => {
+    const article = await Article.findById(id);
+    if (!article) throw new Error('Article not found');
+
+    if (article.author.toString() !== userId) {
+        throw new Error('Not authorized');
+    }
+
+    return await Article.findByIdAndUpdate(id, updateData, { new: true });
+};
+
+const deleteArticle = async (id, userId) => {
+    const article = await Article.findById(id);
+    if (!article) throw new Error('Article not found');
+
+    if (article.author.toString() !== userId) {
+        throw new Error('Not authorized');
+    }
+
+    await Article.findByIdAndDelete(id);
+    return { message: 'Article deleted successfully' };
+};
+
+export default {
+    createArticle,
+    getAllArticles,
+    getArticleById,
+    updateArticle,
+    deleteArticle,
+}
