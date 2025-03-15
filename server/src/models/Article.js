16af299a92c490477fe781744c1a05fed39d7b24
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2025-03-15 20:35:15 +0200

    create article model

---
+++ server/src/models/Article.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const articleSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+export default mongoose.model('Article', articleSchema);
