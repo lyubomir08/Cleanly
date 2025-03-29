088f11650501ba19d76a47613631e36ce3565d62
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2025-03-29 17:07:54 +0200

    add testid to spinner

--- client/src/components/loading-spinner/LoadingSpinner.jsx
+++ client/src/components/loading-spinner/LoadingSpinner.jsx
@@ -1,7 +1,7 @@
 export default function LoadingSpinner() {
     return (
         <div className="flex items-center justify-center h-40">
-            <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-gray-800 border-opacity-50"></div>
+            <div data-testid="loading-spinner" className="animate-spin rounded-full h-16 w-16 border-t-4 border-gray-800 border-opacity-50"></div>
         </div>
     );
 }
