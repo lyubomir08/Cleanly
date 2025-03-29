65dfb98ec251f2576427449039333f21e4da3018
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2025-03-29 15:58:42 +0200

    add unit tests for service item

---
+++ client/src/components/service/service-catalog/service-item/ServiceItem.spec.jsx
@@ -0,0 +1,46 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import ServiceItem from './ServiceItem';
+import { MemoryRouter, useNavigate } from 'react-router';
+
+const navigateMock = vi.fn();
+
+vi.mock('react-router', async () => {
+    const actual = await vi.importActual('react-router');
+    return {
+        ...actual,
+        useNavigate: () => navigateMock
+    }
+});
+
+describe('ServiceItem component', () => {
+    const service = {
+        _id: 'abc123',
+        name: 'Deep Cleaning',
+        price: 80,
+        imageUrl: 'https://image.com',
+        likes: [1, 2],
+        dislikes: []
+    };
+
+    it('should render service data', () => {
+        render(
+            <MemoryRouter>
+                <ServiceItem service={service} />
+            </MemoryRouter>
+        );
+
+        expect(screen.getByText(/deep cleaning/i)).toBeInTheDocument();
+        expect(screen.getByText(/\$80/)).toBeInTheDocument();
+        expect(screen.getByAltText(/deep cleaning/i)).toBeInTheDocument();
+        expect(screen.getByText('2')).toBeInTheDocument();
+        expect(screen.getByText('0')).toBeInTheDocument();
+    });
+
+    it('should navigate on button click', () => {
+        render(<ServiceItem service={service} />, { wrapper: MemoryRouter });
+
+        fireEvent.click(screen.getByRole('button', { name: /details/i }));
+
+        expect(navigateMock).toHaveBeenCalledWith('/services/abc123/details');
+    });
+});
